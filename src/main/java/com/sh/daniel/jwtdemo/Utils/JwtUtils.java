8350cac814a412bc9292e043bd1fe2d1182ca301
zhao hanyang <zhaozezeze@yeah.net>
2018-07-18 16:50:46 +0800

    JWt生成以及测试类

---
+++ src/main/java/com/sh/daniel/jwtdemo/Utils/JwtUtils.java
@@ -0,0 +1,14 @@
+package com.sh.daniel.jwtdemo.Utils;
+
+import com.auth0.jwt.JWT;
+
+public class JwtUtils {
+
+    public static void main(String[] args) {
+
+       // JWT.create().
+
+    }
+
+
+}
